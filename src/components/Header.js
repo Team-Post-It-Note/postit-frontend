0b884a4892a0fe23dfdd0e80c54cb86c84a3e4b8
MIRIAM SILVA <MIRIAM-SILVA@LIVE.COM>
2021-06-29 18:08:32 -0700

    css updates to header, footer, jumbo & app.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,13 @@ import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 import LogoutButton from './LogoutButton.js';
 import { withAuth0 } from '@auth0/auth0-react';
+import './Header.css';
 
 class Header extends React.Component {
   render() {
     return(
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>Our New APP</Navbar.Brand>
+      <Navbar collapseOnSelect expand="lg" className="navBar" variant="dark" >
+        <Navbar.Brand><h1>Where To Go Now</h1></Navbar.Brand>
         <Link to="/">Search</Link>
         {this.props.auth0.isAuthenticated?<Link to="/profile">Profile</Link>:''}
         {this.props.auth0.isAuthenticated
--- src/components/LandingJumbo.js
+++ src/components/LandingJumbo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Jumbotron, Carousel } from 'react-bootstrap';
+import './LandingJumbo.css';
 
 class LandingJumbo extends React.Component {
   render() {
@@ -8,37 +9,37 @@ class LandingJumbo extends React.Component {
       <Jumbotron>
         <Carousel>
           {/* Will map carousel.item to show data */}
-          <Carousel.Item>
+          <Carousel.Item className="carouselItem">
             <img
-              className="d-block w-100"
-              src={`https://via.placeholder.com/800x400/000000/FFFFFF/?text=placeholder`}
+              className="d-block w-65"
+              src="/images/jumboLanding/chanan-greenblatt-wNQ4lFafIfI-unsplash.jpg"
               alt='thing'
             />
-            <Carousel.Caption>
-              <p>Place holding text.  unique name/location here</p>
-            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="d-block w-100"
-              src={`https://via.placeholder.com/800x400/000000/FFFFFF/?text=placeholder`}
+              className="d-block w-65"
+              src="/images/jumboLanding/preston-goff-8lUrRQ8CflI-unsplash.jpg"
               alt='thing'
             />
-            <Carousel.Caption>
-              <p>Place holding text.  unique name/location here</p>
-            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="d-block w-100"
-              src={`https://via.placeholder.com/800x400/000000/FFFFFF/?text=placeholder`}
+              className="d-block w-65"
+              src="/images/jumboLanding/rana-sawalha-W_-6PWGbYaU-unsplash (1).jpg"
               alt='thing'
             />
-            <Carousel.Caption>
-              <p>Place holding text.  unique name/location here</p>
-            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-65"
+              src="/images/jumboLanding/sacha-verheij-5bwgW8_9OPs-unsplash.jpg"
+              alt='thing'
+            />
           </Carousel.Item>
         </Carousel>
+        <h2>Traveling? New To The City? Looking For Something New?</h2>
+        <h4>Let us help you plan your day!</h4>
       </Jumbotron>
     );
   }
