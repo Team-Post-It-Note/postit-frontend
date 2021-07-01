1d725bb9e72aa4dae4c0c79bd08fbe2ac5e399f7
MIRIAM SILVA <MIRIAM-SILVA@LIVE.COM>
2021-07-01 11:14:30 -0700

    clickable urls

--- src/components/javascript/restaurants/SavedRestaurants.js
+++ src/components/javascript/restaurants/SavedRestaurants.js
@@ -63,8 +63,8 @@ class Restaurants extends React.Component {
                     <Card.Text>{rest.name}</Card.Text>
                     <Card.Text>Address: {rest.location}</Card.Text>
                     <Card.Text>Rating: {rest.rating}</Card.Text>
+                    <Card.Text><a href={rest.url}>Click to View Website</a></Card.Text>
                     <Card.Text>Phone: {rest.phone}</Card.Text>
-                    <Card.Text>Website: {rest.url}</Card.Text>
                     <Button variant="danger" onClick={() => this.deleteRest(rest._id)}>Remove</Button>
                   </Card.Footer>
                 )
